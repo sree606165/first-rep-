9fb29059fbfb245f5cd4b6e04d1c650d9a783ad1
Sree Manikandan V <sree606165@gmail.com>
2026-07-22 22:54:16 +0530

    prime.java is added

--- prime.java
+++ prime.java
@@ -1,3 +1,22 @@
-public class prime {
-    
+import java.util.Scanner;
+
+class prime
+{
+    public static void main(String[ ]args)
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter a Number:");
+       int n = sc.nextInt();
+       boolean prime = true;
+       if (n <= 1) prime = false;
+       for (int i = 2 ; i <= Math.sqrt(n); i++)
+       {
+        if( n % i == 0)
+        {
+            prime = false;
+        break;
+       }
+    }
+    System.out.println(prime ? "Prime Number": "Not Prime Number");
+}
 }
